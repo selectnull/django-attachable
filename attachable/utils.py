4b1c5252090fc23b5f60a39d12ef18eb54ae4ee3
Sasha Matijasic <sasha@selectnull.com>
2014-03-18 15:18:48 +0100

    fix add_inlines when ModelAdmin does not define its own inlines

--- attachable/utils.py
+++ attachable/utils.py
@@ -3,6 +3,9 @@ from .admin import AttachmentInline
 
 
 def add_inline(model, model_admin):
-    admin.site.unregister(model)
+    if not 'inlines' in model_admin.__dict__:
+        model_admin.inlines = ()
     model_admin.inlines += (AttachmentInline, )
+
+    admin.site.unregister(model)
     admin.site.register(model, model_admin)
