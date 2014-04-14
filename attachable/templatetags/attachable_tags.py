93d89beb61e7fecca4237040bc7b24de097aa176
Dino Perovic <dino.perovic@gmail.com>
2014-04-14 10:25:58 +0200

    Add version, update templatetag

--- attachable/templatetags/attachable_tags.py
+++ attachable/templatetags/attachable_tags.py
@@ -8,32 +8,22 @@ from collections import defaultdict
 register = template.Library()
 
 
-class AttachmentNode(template.Node):
-    def __init__(self, obj, output_name):
-        self.obj = template.Variable(obj)
-        self.output_name = output_name
+@register.assignment_tag
+def get_attachments(obj, count=None, group=None, object_type=None):
+    filters = {}
 
-    def render(self, context):
-        obj = self.obj.resolve(context)
-        content_type = ContentType.objects.get_for_model(obj)
-        data = Attachment.objects.select_related().\
-            filter(object_id=obj.pk, content_type=content_type).\
-            order_by('group', 'position')
-        result = defaultdict(list)
-        for x in data:
-            result[x.group].append(x)
-            
-        context[self.output_name] = result
-        return ''
+    if count:
+        count = int(count)
+    if group:
+        filters['group'] = group
+    if object_type:
+        filters['object_type'] = object_type
 
-@register.tag
-def get_attachments(parser, token):
-    """
-    {% get_attachments obj as output_name %}
+    content_type = ContentType.objects.get_for_model(obj)
+    data = Attachment.objects.select_related().\
+        filter(content_type=content_type, object_id=obj.pk, **filters).\
+        order_by('group', 'position')
 
-    """
-    try:
-        tag_name, obj, _as, output_name = token.split_contents()
-    except ValueError:
-        raise template.TemplateSyntaxError('{} tag takes exactly 3 arguments'.format(tag_name))
-    return AttachmentNode(obj, output_name)
+    if data:
+        return data if not count else data[:count] if count > 1 else data[0]
+    return None
